c2bf964bc6582cfe9c52d587a06524d8a79adf78
coreyeross25 <coreyeross12@gmail.com>
2024-07-14 01:02:17 -0400

    Finished

--- src/main/java/zipcode/rocks/Main.java
+++ src/main/java/zipcode/rocks/Main.java
@@ -1,5 +1,7 @@
 package zipcode.rocks;
 
+import jdk.internal.jimage.ImageStrings;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -17,7 +19,7 @@ public class Main {
         // Load data file into an ArrayList<Person> of Person objects.
         Main programObject = new Main();
 
-        List listOfStudents = programObject.loadCSVFile(student_source);
+        List listOfStudents = programObject.handWrittenLoadOfData();
 
         int numberOfPersons = listOfStudents.size();
         for (int i = 0; i< numberOfPersons; i++){
@@ -46,6 +48,104 @@ public class Main {
         String[] values = line.split(COMMA_DELIMITER);
         Person newPerson = new Person(values[0], values[1]);
         return newPerson;
+    }
+
+    private List handWrittenLoadOfData() {
+        ArrayList<Person> students = new ArrayList<>();
+
+        Person newPersonCoreye = new Person("Coreye", "Ross");
+        newPersonCoreye.setFavoriteBeverage("Water");
+        newPersonCoreye.setFavoriteColor("Black");
+        newPersonCoreye.setBirthMonth("October");
+        newPersonCoreye.setHavePets(false);
+        students.add(newPersonCoreye);
+
+        Person newPersonWill = new Person("Will", "Chapman");
+        newPersonWill.setFavoriteBeverage("Water");
+        newPersonWill.setFavoriteColor("Purple");
+        newPersonWill.setBirthMonth("December");
+        newPersonWill.setHavePets(false);
+        students.add(newPersonWill);
+
+        Person newPersonJosue = new Person("Josue", "Castro");
+        newPersonJosue.setFavoriteBeverage("Coffee");
+        newPersonJosue.setFavoriteColor("Blue");
+        newPersonJosue.setBirthMonth("June");
+        newPersonJosue.setHavePets(true);
+        students.add(newPersonJosue);
+
+        Person newPersonMo = new Person("Mohammed", "N");
+        newPersonMo.setFavoriteBeverage("Snapple");
+        newPersonMo.setFavoriteColor("Green");
+        newPersonMo.setBirthMonth("August");
+        newPersonMo.setHavePets(false);
+        students.add(newPersonMo);
+
+        Person newPersonJared = new Person("Jared", "Thacker");
+        newPersonJared.setFavoriteBeverage("Water");
+        newPersonJared.setFavoriteColor("Green");
+        newPersonJared.setBirthMonth("December");
+        newPersonJared.setHavePets(false);
+        students.add(newPersonJared);
+
+        Person newPersonJonathan = new Person("Jonathan", "Diehl");
+        newPersonJonathan.setFavoriteBeverage("Coffe");
+        newPersonJonathan.setFavoriteColor("Green");
+        newPersonJonathan.setBirthMonth("February");
+        newPersonJonathan.setHavePets(true);
+        students.add(newPersonJonathan);
+
+        Person newPersonBryan = new Person("Bryan", "Smith");
+        newPersonBryan.setFavoriteBeverage("Water");
+        newPersonBryan.setFavoriteColor("Brown");
+        newPersonBryan.setBirthMonth("May");
+        newPersonBryan.setHavePets(false);
+        students.add(newPersonJonathan);
+
+
+        Person newPersonIan = new Person("Ian", "Gordon");
+        newPersonIan.setFavoriteBeverage("Water");
+        newPersonIan.setFavoriteColor("Green");
+        newPersonIan.setBirthMonth("April");
+        newPersonIan.setHavePets(true);
+        students.add(newPersonJonathan);
+
+        Person newPersonDiptika = new Person("Diptika", "Devi");
+        newPersonDiptika.setFavoriteBeverage("Tea");
+        newPersonDiptika.setFavoriteColor("Orange");
+        newPersonDiptika.setBirthMonth("June");
+        newPersonDiptika.setHavePets(false);
+        students.add(newPersonJonathan);
+
+        Person newPersonQian = new Person("Qian", "Wang");
+        newPersonQian.setFavoriteBeverage("Tea");
+        newPersonQian.setFavoriteColor("Orange");
+        newPersonQian.setBirthMonth("June");
+        newPersonQian.setHavePets(false);
+        students.add(newPersonQian);
+
+        Person newPersonTim = new Person("Tim", "Linkous");
+        newPersonTim.setFavoriteBeverage("Water");
+        newPersonTim.setFavoriteColor("Green");
+        newPersonTim.setBirthMonth("September");
+        newPersonTim.setHavePets(false);
+        students.add(newPersonTim);
+
+        Person newPersonPeter = new Person("Peter", "Beever");
+        newPersonPeter.setFavoriteBeverage("Latte");
+        newPersonPeter.setFavoriteColor("Purple");
+        newPersonPeter.setBirthMonth("February");
+        newPersonPeter.setHavePets(true);
+        students.add(newPersonPeter);
+
+        Person newPersonTeddy = new Person("Teddy", "Archibald");
+        newPersonTeddy.setFavoriteBeverage("Half&Half");
+        newPersonTeddy.setFavoriteColor("Red");
+        newPersonTeddy.setBirthMonth("March");
+        newPersonTeddy.setHavePets(false);
+        students.add(newPersonTeddy);
+
+        return students;
     }
 
 }
--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -3,10 +3,14 @@ package zipcode.rocks;
 public class Person {
     private String firstname;
     private String lastname;
+    private String favoriteBeverage;
+    private String favoriteColor;
+    private String birthMonth;
+    private boolean havePets;
 
-    public Person(String fname, String lname) {
-        this.firstname = fname;
-        this.lastname = lname;
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
 
@@ -14,19 +18,53 @@ public class Person {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
+    public String setFirstname(String firstname) {
         this.firstname = firstname;
+        return firstname;
     }
 
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
+    public String setLastname(String lastname) {
         this.lastname = lastname;
+        return lastname;
     }
 
     public String toString() {
         return this.lastname + ", " + this.firstname;
+    }
+
+    public boolean getHavePets() {
+        return havePets;
+    }
+
+    public void setHavePets(boolean havePets) {
+        this.havePets = havePets;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public String getFavoriteColor() {
+        return favoriteColor;
+    }
+
+
+    public void setFavoriteColor(String favoriteColor) {
+        this.favoriteColor = favoriteColor;
+    }
+
+    public String getFavoriteBeverage() {
+        return favoriteBeverage;
+    }
+
+    public void setFavoriteBeverage(String favoriteColor) {
+        this.favoriteBeverage = favoriteBeverage;
     }
 }
